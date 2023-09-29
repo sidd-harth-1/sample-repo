744046cf2ab13d73337355cc69edcf098c2d6a26
Siddharth <128633880+sidd-harth-1@users.noreply.github.com>
2023-09-29 14:35:31 +0530

    added new steps

--- validate-script.sh
+++ validate-script.sh
@@ -4,23 +4,21 @@ REPO=$2
 BRANCH=$3
 WORKFLOW_NAME=$4
 
+#  Get the workflow YAML content
+WORKFLOW_YAML=$(curl -X GET  -sk -H "Accept: application/vnd.github+json" \
+https://raw.githubusercontent.com/$OWNER/$REPO/$BRANCH/.github/workflows/$WORKFLOW_NAME)
 
-# Get the workflow YAML content
-wget -c -q -O workflow.yml https://raw.githubusercontent.com/$OWNER/$REPO/$BRANCH/.github/workflows/$WORKFLOW_NAME
-
-# Validate the workflow YAML
-#Check if the workflow has two jobs named testing and deployment
-if ! (yq '.jobs | keys' workflow.yml  |  grep -q 'testing') && (yq '.jobs | keys' workflow.yml |  grep -q 'deploying'); then
+# Check if the workflow has two jobs named testing and deployment
+if ! (yq '.jobs | keys' <<< $WORKFLOW_YAML  |  grep -q 'testing') && (yq '.jobs | keys' <<< $WORKFLOW_YAML |  grep -q 'deploying'); then
 echo "The workflow does not have two jobs named testing and deployment."
 exit 1
 fi
 
-# #Check if the testing job has the on-error-continue property set to true
-if ! yq eval '.jobs.testing.continue-on-error'  workflow.yml | grep -v "null"; then
-echo "ERROR: The testing job does not have the on-error-continue property"
+# Check if the testing job has the continue-on-error property
+if ! yq eval '.jobs.testing.continue-on-error' <<< $WORKFLOW_YAML | grep -v "null"; then
+echo "ERROR: The testing job does not have the continue-on-error property"
 exit 1
 fi
 
-# The workflow is valid
-echo "The workflow is valid."
+echo "The workflow Passed all the tests"
 exit 0
